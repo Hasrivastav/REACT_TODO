23ecf4019101bef22680a3f0f8207c91091a3a0f
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-04-08 12:00:30 +0530

    initial commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const TodoItem = ({title,
+    description,
+    isCompleted,
+    deleteHandler, 
+    updateHandler,
+    id }) => {
+  return (
+    <div className='todo'>
+<div>
+    <h4>{title}</h4>
+    <p>{description}</p>
+</div>
+<div>
+    <input onChange={()=>updateHandler(id)} type="checkbox" checked={isCompleted}/>
+    <button onClick={()=>deleteHandler(id)} className='btn'>Delete</button>
+</div>
+
+    </div>
+  )
+}
+
+export default TodoItem
